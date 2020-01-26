90a1e2ba7ab8379f6cfb135542b3c110e717c419
Emanuel Rocha <emanuel.rochax@gmail.com>
2020-01-26 19:14:36 -0300

    Removendo usings não utilizados

--- src/GBChallenge.Core/BusinessServices/CompraService.cs
+++ src/GBChallenge.Core/BusinessServices/CompraService.cs
@@ -5,7 +5,6 @@ using GBChallenge.Domain.SimpleTypes;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GBChallenge.Core.BusinessServices
