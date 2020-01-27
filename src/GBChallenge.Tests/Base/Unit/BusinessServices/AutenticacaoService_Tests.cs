d06bbd6dd0f3d071b79e6e7ad2573dab0a339b59
Emanuel Rocha <emanuel.rocha@capgemini.com>
2020-01-27 16:31:14 -0300

    Adding Test Project

---
+++ src/GBChallenge.Tests/Base/Unit/BusinessServices/AutenticacaoService_Tests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBChallenge.Tests.Base.Unit.BusinessServices
+{
+    class AutenticacaoService_Tests
+    {
+    }
+}
---
+++ src/GBChallenge.Tests/Base/Unit/BusinessServices/CompraService_Tests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBChallenge.Tests.Base.Unit.BusinessServices
+{
+    class CompraService_Tests
+    {
+    }
+}
